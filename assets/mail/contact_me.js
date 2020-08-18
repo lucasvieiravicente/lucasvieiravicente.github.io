8bb5f5d49235ea3ab4b998638115a636d144647e
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2020-08-18 11:44:33 -0300

    ...

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -31,51 +31,47 @@ $(function () {
                 contentType: "application/json",      
                 dataType: "application/json",
                 cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Sua mensagem foi enviada. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                error: function (data) {
-                    // Fail message
-                    let messageError;
-                    
-                    if(data.responseText != null){
-                        messageError = data.responseText;
-                    }
-                    else{
-                        messageError = "Desculpa " + firstName + ", parece que no momento não consigo enviar o e-mail, tente outro contato!";
-                    }
+            }).done(function(data){
+                // Success message
+                $("#success").html("<div class='alert alert-success'>");
+                $("#success > .alert-success")
+                    .html(
+                        "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    )
+                    .append("</button>");
+                $("#success > .alert-success").append(
+                    "<strong>" + data.responseText + "</strong>"
+                );
+                $("#success > .alert-success").append("</div>");                
+            }).fail(function(data){
+                // Fail message
+                let messageError;
+                                
+                if(data.responseText != null){
+                    messageError = data.responseText;
+                }
+                else{
+                    messageError = "Desculpa " + firstName + ", parece que no momento não consigo enviar o e-mail, tente outro contato!";
+                }
 
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(messageError)
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
-                },
-            });            
+                $("#success").html("<div class='alert alert-danger'>");
+                $("#success > .alert-danger")
+                    .html(
+                        "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    )
+                    .append("</button>");
+                $("#success > .alert-danger").append(
+                    $("<strong>").text(messageError)
+                );
+                $("#success > .alert-danger").append("</div>");                
+            }).always(function(){
+                //clear all fields
+                $("#contactForm").trigger("reset");
+
+                setTimeout(function () {
+                    $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                }, 1000);
+            });                                                
         },
         filter: function () {
             return $(this).is(":visible");
